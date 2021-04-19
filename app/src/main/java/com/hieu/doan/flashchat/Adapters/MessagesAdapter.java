917d36c5db0b2c5cdfd6be5d41ec11a334ed7ece
hieutravinhaxe <hieutravinhaxe@gmail.com>
2021-04-19 13:47:30 +0700

    load file+image receive

--- app/src/main/java/com/hieu/doan/flashchat/Adapters/MessagesAdapter.java
+++ app/src/main/java/com/hieu/doan/flashchat/Adapters/MessagesAdapter.java
@@ -1,6 +1,10 @@
 package com.hieu.doan.flashchat.Adapters;
 
+import android.app.DownloadManager;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,12 +17,20 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.hieu.doan.flashchat.Activities.ChatActivity;
 import com.hieu.doan.flashchat.Models.Message;
 import com.hieu.doan.flashchat.R;
 
+import java.io.File;
 import java.util.ArrayList;
+
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
 
 public class MessagesAdapter extends RecyclerView.Adapter {
 
@@ -26,6 +38,7 @@ public class MessagesAdapter extends RecyclerView.Adapter {
     final int RECEIVE = 2;
 
     Context context;
+    FirebaseStorage storage = FirebaseStorage.getInstance();
     ArrayList<Message> messages;
 
 
@@ -61,7 +74,7 @@ public class MessagesAdapter extends RecyclerView.Adapter {
 
     @Override
     public void onBindViewHolder(@NonNull final RecyclerView.ViewHolder holder, final int position) {
-        Message message = messages.get(position);
+        final Message message = messages.get(position);
 
         if(holder.getClass() == SendViewHolder.class){
             SendViewHolder sendViewHolder = (SendViewHolder) holder;
@@ -105,22 +118,42 @@ public class MessagesAdapter extends RecyclerView.Adapter {
                 receiveViewHolder.receiveTV.setText(message.getMsg());
             }
         }
-        /*holder.itemView.setOnClickListener(new View.OnClickListener() {
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(holder.getItemViewType() == SEND){
                     SendViewHolder sendViewHolder = (SendViewHolder) holder;
-                    if(sendViewHolder.sendTV.equals("file123456hvcseblhvjblasfv")){
-                        Log.i("CHECK","FILE");
+                    if(sendViewHolder.sendTV.getVisibility() == View.VISIBLE){
+                        //Log.i("CHECK","text");
                     }
-                    else if(sendViewHolder.sendTV.equals("photofefededeofkt")){
-                        Log.i("CHECK","IMAGE");
+                    else if(sendViewHolder.image.getVisibility() == View.VISIBLE){
+                        //Log.i("CHECK","IMAGE");
+                    }
+                    else {
+                        //Log.i("CHECK","File");
                     }
                 }else{
-
+                    ReceiveViewHolder receiveViewHolder = (ReceiveViewHolder) holder;
+                    if(receiveViewHolder.receiveTV.getVisibility() == View.VISIBLE){
+                        //Log.i("CHECK","text");
+                    }
+                    else if(receiveViewHolder.image.getVisibility() == View.VISIBLE){
+                        //Log.i("CHECK",message.getImageUri());
+                        //Log.i("NAME ",fName);
+                        //Log.i("EX",fEx);
+                        downloadFile(holder.itemView.getContext(),"image",".png",DIRECTORY_DOWNLOADS,message.getImageUri());
+                    }
+                    else {
+                        //Log.i("CHECK",message.getFileUri());
+                        String fName = message.getFileName().substring(0,message.getFileName().lastIndexOf("."));
+                        String fEx =message.getFileName().substring(message.getFileName().lastIndexOf("."));
+                        //Log.i("NAME ",fName);
+                        //Log.i("EX",fEx);
+                        downloadFile(holder.itemView.getContext(),fName,fEx,DIRECTORY_DOWNLOADS,message.getFileUri());
+                    }
                 }
             }
-        });*/
+        });
     }
 
     @Override
@@ -154,5 +187,17 @@ public class MessagesAdapter extends RecyclerView.Adapter {
             image = itemView.findViewById(R.id.imageRecieve);
             fileReceive = itemView.findViewById(R.id.attachFileRecieve);
         }
+    }
+    public void downloadFile(Context context, String fileName, String fileExtension, String destinationDirectory, String url) {
+
+        DownloadManager downloadmanager = (DownloadManager) context.
+                getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName + fileExtension);
+
+        downloadmanager.enqueue(request);
     }
 }
