cb27de9d82270292595f4b1c0019d7e5fb2f06b3
phong6786789 <61493358+phong6786789@users.noreply.github.com>
2021-04-27 17:58:44 +0700

    no message

--- app/src/main/java/com/hieu/doan/flashchat/call_api/notification/Notification.java
+++ app/src/main/java/com/hieu/doan/flashchat/call_api/notification/Notification.java
@@ -43,11 +43,50 @@ public class Notification {
             mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         }
 
+
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, (int) (System.currentTimeMillis() & 0xfffffff),
+                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         FirebaseDatabase.getInstance().getReference().child("users").child(from).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 User user  = snapshot.getValue(User.class);
                 name = user.getName();
+                NotificationCompat.Builder notificationBuilder =
+                        new NotificationCompat.Builder(context, CALL_CHANNEL_ID)
+                                .setSmallIcon(R.mipmap.ic_launcher)
+                                .setSound(null)
+                                .setContentTitle("Đang có cuộc gọi đến")
+                                .setContentText("từ: " + name)
+                                .setPriority(NotificationCompat.PRIORITY_MAX)
+                                .setCategory(NotificationCompat.CATEGORY_CALL)
+                                .setContentIntent(pendingIntent);
+
+                android.app.Notification incomingCallNotification = notificationBuilder.build();
+
+                mNotificationManager.notify(44448888, incomingCallNotification);
+
+                Utils.runOnUithread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (Common.audioManager == null) {
+                            Common.audioManager = StringeeAudioManager.create(context);
+                            Common.audioManager.start(new StringeeAudioManager.AudioManagerEvents() {
+                                @Override
+                                public void onAudioDeviceChanged(StringeeAudioManager.AudioDevice selectedAudioDevice, Set<StringeeAudioManager.AudioDevice> availableAudioDevices) {
+                                }
+                            });
+                        }
+
+                        if (Common.ringtone == null) {
+                            Uri ringtoneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+                            Common.ringtone = RingtoneManager.getRingtone(context, ringtoneUri);
+                            Common.ringtone.play();
+                        }
+                    }
+                });
             }
 
             @Override
@@ -56,43 +95,5 @@ public class Notification {
             }
         });
 
-        Intent intent = new Intent(context, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, (int) (System.currentTimeMillis() & 0xfffffff),
-                intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(context, CALL_CHANNEL_ID)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setSound(null)
-                        .setContentTitle("Đang có cuộc gọi đến")
-                        .setContentText("từ: " + name)
-                        .setPriority(NotificationCompat.PRIORITY_MAX)
-                        .setCategory(NotificationCompat.CATEGORY_CALL)
-                        .setContentIntent(pendingIntent);
-
-        android.app.Notification incomingCallNotification = notificationBuilder.build();
-
-        mNotificationManager.notify(44448888, incomingCallNotification);
-
-        Utils.runOnUithread(new Runnable() {
-            @Override
-            public void run() {
-                if (Common.audioManager == null) {
-                    Common.audioManager = StringeeAudioManager.create(context);
-                    Common.audioManager.start(new StringeeAudioManager.AudioManagerEvents() {
-                        @Override
-                        public void onAudioDeviceChanged(StringeeAudioManager.AudioDevice selectedAudioDevice, Set<StringeeAudioManager.AudioDevice> availableAudioDevices) {
-                        }
-                    });
-                }
-
-                if (Common.ringtone == null) {
-                    Uri ringtoneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-                    Common.ringtone = RingtoneManager.getRingtone(context, ringtoneUri);
-                    Common.ringtone.play();
-                }
-            }
-        });
     }
 }
